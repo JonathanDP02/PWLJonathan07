d3bbcf9d2a8b7a616fa75adf652a4d01e1a28534
JonathanDP02 <jonatanpangestu23@gmail.com>
2026-03-02 23:19:24 +0700

    p3

--- pertemuan-3/routes/web.php
+++ pertemuan-3/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +12,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
