3fc28aa850f389aba07695ffc82f9a0e88292db7
JonathanDP02 <jonatanpangestu23@gmail.com>
2026-03-08 21:43:58 +0700

    praktikum 1

--- pertemuan-3/app/Http/Controllers/UserController.php
+++ pertemuan-3/app/Http/Controllers/UserController.php
@@ -9,6 +9,14 @@ class UserController extends Controller
 {
     public function index()
     {
+    
+    $data = [
+        'level_id' => 2,
+        'username' => 'manager_tiga',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345')
+    ];
+    
     $data = UserModel::all();
     return view('user', ['data' => $data]);
     }
---
+++ pertemuan-3/public/user.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<h1>Halaman User</h1>";
+echo "<p>Selamat datang di halaman user Laravel.</p>";
+
+$name = "Jojo";
+
+echo "<p>Nama User: " . $name . "</p>";
+
+?>
