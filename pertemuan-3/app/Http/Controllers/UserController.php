a01478f89485b6519801eb16fdcfeee7ae08f451
JonathanDP02 <jonatanpangestu23@gmail.com>
2026-03-08 22:05:20 +0700

    praktikum 2.2

--- pertemuan-3/app/Http/Controllers/UserController.php
+++ pertemuan-3/app/Http/Controllers/UserController.php
@@ -12,7 +12,9 @@ class UserController extends Controller
     
     // $user = UserModel::find(1);
     // $user = UserModel::firstWhere('level_id', 1);
-    $user = UserModel::findOr (1, ['username', 'nama'], function () {abort(404);});
+    // $user = UserModel::findOr (1, ['username', 'nama'], function () {abort(404);});
+    // $user = UserModel::findOrFail(1);
+    $user = UserModel::where('username', 'manager9')->firstOrFail();
     return view('user', ['data' => $user]);
     }
 }
