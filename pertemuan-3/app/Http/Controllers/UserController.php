0424e6732d7674dc5a83445a31dfc1fcb0bb6b66
JonathanDP02 <jonatanpangestu23@gmail.com>
2026-03-08 22:32:42 +0700

    praktikum 2.4

--- pertemuan-3/app/Http/Controllers/UserController.php
+++ pertemuan-3/app/Http/Controllers/UserController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-    
-    // $user = UserModel::find(1);
-    // $user = UserModel::firstWhere('level_id', 1);
-    // $user = UserModel::findOr (1, ['username', 'nama'], function () {abort(404);});
-    // $user = UserModel::findOrFail(1);
-    // $user = UserModel::where('username', 'manager9')->firstOrFail();
-    $user = UserModel::where('level_id', 2)->count();
-    return view('user', compact('user'));
+
+    $user = UserModel::firstOrCreate(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
+    );
+    // $user->save();
+    return view('user', ['data' => $user]);
     }
 }
