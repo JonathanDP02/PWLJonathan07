dc4fc66bfb4c5ebbbbd3c17e948ba91001ddf444
JonathanDP02 <jonatanpangestu23@gmail.com>
2026-03-08 22:16:26 +0700

    praktikum 2.3

--- pertemuan-3/app/Http/Controllers/UserController.php
+++ pertemuan-3/app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
     // $user = UserModel::firstWhere('level_id', 1);
     // $user = UserModel::findOr (1, ['username', 'nama'], function () {abort(404);});
     // $user = UserModel::findOrFail(1);
-    $user = UserModel::where('username', 'manager9')->firstOrFail();
-    return view('user', ['data' => $user]);
+    // $user = UserModel::where('username', 'manager9')->firstOrFail();
+    $user = UserModel::where('level_id', 2)->count();
+    return view('user', compact('user'));
     }
 }
--- pertemuan-3/resources/views/user.blade.php
+++ pertemuan-3/resources/views/user.blade.php
@@ -5,23 +5,13 @@
 </head>
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    <table border="1" cellpadding="5">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>Jumlah Pengguna</td>
         </tr>
-
-        @foreach ($data as $d)
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>  
+            <td>{{ $user }}</td>
         </tr>
-        @endforeach
-
     </table>
 </body>
 </html>
